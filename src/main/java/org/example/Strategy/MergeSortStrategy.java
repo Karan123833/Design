ab4b0911706b1a1ea5e61059bb75fbda26a024fd
Karan Dua <karan@Karans-MacBook-Air.local>
2025-09-01 18:32:36 +0530

    Written mergestrategy

--- src/main/java/org/example/Strategy/MergeSortStrategy.java
+++ src/main/java/org/example/Strategy/MergeSortStrategy.java
@@ -1,4 +1,45 @@
 package org.example.Strategy;
 
-public class MergeSortStrategy {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeSortStrategy implements Strategy{
+
+    public void merge(int low,int mid,int high,List<Integer> list) {
+        List<Integer> temp = new ArrayList<>();
+        int i=low;
+        int j=mid+1;
+        while(i<=mid&&j<=high) {
+            if(list.get(i)<= list.get(j)) {
+                temp.add(list.get(i));
+                i++;
+            }
+            else {
+                temp.add(list.get(j));
+                j++;
+            }
+        }
+        while(i<=mid) {
+            temp.add(list.get(i++));
+        }
+        while(j<=high) {
+            temp.add(list.get(j++));
+        }
+        for (int k = 0; k < temp.size(); k++) {
+            list.set(low + k, temp.get(k));
+        }
+    }
+    public void mergesort(int low,int high,List<Integer> list) {
+        if(low<high) {
+            int mid = (low + high) >> 1;
+            mergesort(low, mid, list);
+            mergesort(mid + 1, high, list);
+
+            merge(low,mid,high,list);
+        }
+    }
+    @Override
+    public void sort(List<Integer> list) {
+        mergesort(0, list.size()-1,list);
+    }
 }
--- src/main/java/org/example/Strategy/main.java
+++ src/main/java/org/example/Strategy/main.java
@@ -7,9 +7,10 @@ import java.util.List;
 public class main {
     public static void main(String[] args) {
         Strategy strategy = new BubbleSortStrategy();
-        List<Integer> list = Arrays.asList(2,1,5,4);
+        List<Integer> list = Arrays.asList(2,1,5,4,1,2,1,2,3,5);
         Sorter sorter = new Sorter(strategy);
         sorter.setStrategy(new QuickSortStrategy());
+        sorter.setStrategy(new MergeSortStrategy());
         sorter.sortkaro(list);
         System.out.println(list);
     }
